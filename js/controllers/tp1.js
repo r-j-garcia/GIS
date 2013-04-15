855075726829c7a540107acb119578d5c5b0dba0
Lea Marty <leanmarty@gmail.com>
2013-04-15 03:35:30 -0300

    Puse el mapa. Hice addMarkers y trazar la linea. Metí un botón para reiniciar los markers y la linea.

--- js/controllers/tp1.js
+++ js/controllers/tp1.js
@@ -2,33 +2,50 @@ angular.module('GIS')
 .controller('Tp1Ctrl', ['$scope', function($scope) {
 	$scope.map = {};
 	$scope.markers = [];
+	$scope.line = {};
 
 	$scope.mapOptions = {
 		center: new google.maps.LatLng(-34.5986488535611, -58.42001974582672),
-		zoom: 15,
+		zoom: 13,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 
+	$scope.resetMarkers = function() {
+		deleteLine();
+		angular.forEach($scope.markers, function(marker) {
+			marker.setMap(null);
+		});
+		$scope.markers = [];
+	};
+
+	var deleteLine = function() {
+		if (!$.isEmptyObject($scope.line)) {
+			$scope.line.setMap(null);
+		}
+	};
+
+	var drawLine = function() {
+		if ($scope.markers.length == 2) {
+			deleteLine();
+			$scope.line = new google.maps.Polyline({
+				path: [$scope.markers[0].getPosition(), $scope.markers[1].getPosition()],
+				map: $scope.map
+			})
+		}
+	};
 
 	$scope.addMarker = function($event) {
-		debugger;
-		$scope.markers.push(new google.maps.Marker({
-			map: $scope.map,
-			position: $event.latLng
-		}));
-	};
-
-	$scope.openMarkerInfo = function(marker) {
-		$scope.currentMarker = marker;
-		$scope.currentMarkerLat = marker.getPosition().lat();
-		$scope.currentMarkerLng = marker.getPosition().lng();
-	};
-
-	$scope.setMarkerPosition = function(marker, lat, lng) {
-		marker.setPosition(new google.maps.LatLng(lat, lng));
-	};
-
-	$scope.event = function(a, b, c) {
-		debugger;
+		if ($scope.markers.length < 2) {
+			var marker = new google.maps.Marker({
+				draggable: true,
+				map: $scope.map,
+				position: $event.latLng
+			});
+			marker.position_changed = function() {
+				drawLine();
+			};
+			$scope.markers.push(marker);
+			drawLine();
+		}
 	};
 }]);
